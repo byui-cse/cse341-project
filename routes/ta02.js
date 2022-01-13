0c80a7aebd6229edc2bf4c1534bbcd0c6244fdbb
John Cornia <john.cornia.1@gmail.com>
2022-01-13 11:55:34 -0700

    updates for team activity

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,27 @@
 const express = require('express');
 const router = express.Router();
 
+let userList = [];
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
-    title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-  });
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        path: '/ta02', // For pug, EJS
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+        users: userList
+    });
 });
+
+router.post('/addUser', (req, res, next) => {
+    userList.push({ name: req.body.userName });
+    res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+    userList = userList.filter((user) => user.name != req.body.remove);
+    res.redirect('/ta02');
+});
+
 
 module.exports = router;
